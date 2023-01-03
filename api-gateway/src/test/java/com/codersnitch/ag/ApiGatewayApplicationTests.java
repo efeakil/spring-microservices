3113a6c0d6dd93432a041456ed2cf19aab0f87a9
efeakil <efe.akil35@gmail.com>
2023-01-03 17:43:13 +0300

    api-gateway added

---
+++ api-gateway/src/test/java/com/codersnitch/ag/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codersnitch.ag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
