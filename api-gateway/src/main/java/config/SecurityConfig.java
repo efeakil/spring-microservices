52dbc1ee9f2d90b57617e1fb6deaabc951bfab30
efeakil <efe.akil35@gmail.com>
2023-02-02 20:17:55 +0300

    tried to fix the 401 unauthorized issue when using api-gateway

--- api-gateway/src/main/java/config/SecurityConfig.java
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -12,13 +12,15 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 	
 	@Bean
-	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-		
-		serverHttpSecurity.csrf()
-				.disable()
-				.authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
-				.permitAll().anyExchange().authenticated())
-				.oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-		return serverHttpSecurity.build();
-	}
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf().disable()
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+    }
 }
